c1c9e04d9979baeaca27d7b0915adba82ccdc639
MyTruQs <raulfernandes.rm@gmail.com>
2024-07-22 09:07:01 -0300

    third commit

--- src/pages/trip-details/important-links.tsx
+++ src/pages/trip-details/important-links.tsx
@@ -16,15 +16,6 @@ export function ImportantLinks() {
                     </div>
                     <Link2 className="size-5 text-zinc-400 shrink-0" />
                 </div>
-                <div className="flex items-center justify-between gap-4">
-                    <div className="space-y-1.5">
-                        <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
-                        <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
-                            https://www.airbnb.com.br/rooms/104700011
-                        </a>
-                    </div>
-                    <Link2 className="size-5 text-zinc-400 shrink-0" />
-                </div>
             </div>
 
             <Button variant="secondary" size='full'>
